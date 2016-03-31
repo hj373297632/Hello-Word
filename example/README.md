0698ab821799577fc0c02c0f84d4e4222bf388b8
hujing.huang <hujing.huang@qunar.com>
2016-03-31 11:36:15 +0800

    maven project commit.

---
+++ example/README.md
@@ -0,0 +1,4 @@
+# First GitHub
+Jing Huang First code in GitHub.
+
+First New Branch Add.
---
+++ example/src/main/java/ExecuteMain.java
@@ -0,0 +1,55 @@
+/**
+ * Created by hujing.huang on 2016/2/25.
+ */
+
+class Cat{
+    int color,weight,height;
+
+    Cat(int color,int weight,int height){
+        this.color=color;
+        this.weight=weight;
+        this.height=height;
+    }
+
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        }else {
+            if (obj instanceof Cat){
+                Cat c=(Cat)obj;
+                if (c.color == this.color && c.weight == this.weight && c.height == this.height)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+public class ExecuteMain {
+
+    public static void main(String[] args){
+        OverWriteTest a=new OverWriteTest();
+/*        System.out.println(a.getinfo());
+        a.f();*/
+        a.x();
+
+        Cat c1=new Cat(1,2,3);
+        Cat c2=new Cat(1,2,3);
+        String s1=new String("hello");
+        String s2=new String("hello");
+        System.out.println(c1==c2);
+        System.out.println(c1.equals(c2));
+
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+
+        GoldenMonkey k1=new GoldenMonkey();
+        System.out.println(k1.getMoney());
+        System.out.println("add commit..");
+
+
+    }
+
+
+}
